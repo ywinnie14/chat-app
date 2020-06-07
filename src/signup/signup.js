25d96df3b64984e204effd2b2861419a314fd52d
ywinnie14 <winnieyu1190@gmail.com>
2020-06-07 23:31:40 +1000

    Create sign up page with email, password, password confirmation, submit and error messages. Able to store users info into firebase.

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -12,8 +12,106 @@ import Button from '@material-ui/core/Button';
 const firebase = require("firebase");
 
 class SignupComponent extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            email: null,
+            password: null,
+            passwordConfirmation: null,
+            signupError: ''
+        };
+    }
+
     render(){
-        return (<div>Hello World from Signup</div>);
+        const { classes } = this.props;
+        return (
+            <main className={classes.main}>
+                <CssBaseline></CssBaseline>
+                <Paper className={classes.paper}>
+                    <Typography component='h1' variant='h5'>
+                        Sign Up
+                    </Typography>
+                    <form onSubmit={(e) =>this.submitSignup(e) } className={classes.form}>
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor='signup-email-input'>Enter Your Email</InputLabel>
+                            <Input autoComplete='email' autoFocus onChange={(e) => this.userTyping('email', e)} id='signup-email-input'></Input>
+                        </FormControl>
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor='signup-password-input'>Create A Password</InputLabel>
+                            <Input type='password' onChange={(e) => this.userTyping('password', e)} id='signup-password-input'></Input>
+                        </FormControl>
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor='signup-password-confirmation-input'>Confirm Your Password</InputLabel>
+                            <Input type='password' onChange={(e) => this.userTyping('passwordConfirmation', e)} id='signup-password-confirmation-input'></Input>
+                        </FormControl>
+                        <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>Submit</Button>
+                    </form>
+
+                    {
+                        this.state.signupError ?
+                        <Typography component='h5' variant='h6' className={classes.errorText}>
+                            {this.state.signupError}
+                        </Typography> :
+                        null
+                    }
+
+                    <Typography component='h5' variant='h6' className={classes.hasAccountHeader}>Already Have An Account?</Typography>
+                    <Link className={classes.logInLink} to='/login'>Log In!</Link>
+                </Paper>
+
+            </main>
+        );
+    }
+
+    formIsValid = () => this.state.password === this.state.passwordConfirmation;
+
+    userTyping = (type, e) => {
+        switch (type) {
+            case 'email':
+                this.setState({ email: e.target.value});
+                break;
+            case 'password':
+                this.setState({password: e.target.value});
+                break;
+            case 'passwordConfirmation':
+                this.setState({passwordConfirmation: e.target.value});
+                break;
+            default:
+                break;
+        }
+    }
+
+    submitSignup = (e) => {
+        e.preventDefault();
+
+        if(!this.formIsValid()){
+            this.setState({signupError: 'Passwords do not match!'});
+            return;
+        }
+
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(authRes => {
+            const userObj = {
+                email: authRes.user.email
+            };
+            firebase
+                .firestore()
+                .collection('users')
+                .doc(this.state.email)
+                .set(userObj)
+                .then(() => {
+                    this.props.history.push('/dashboard')
+                }, dbError => {
+                    console.log(dbError);
+                    this.setState({signupError: 'Failed to add user' });
+                })
+        }, authError => {
+            console.log(authError);
+            this.setState({signupError: 'Failed to add user' });
+        })
     }
 }
 
