220945edcf49363ea554434d8f7907d44edb3d2d
ywinnie14 <winnieyu1190@gmail.com>
2020-06-10 19:45:55 +1000

    Create log in page, dashboard page and display initial message.

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import ChatListComponent from '../chatlist/chatList';
+const firebase = require("firebase");
 
 class DashboardComponent extends React.Component {
+    constructor(){
+        super();
+        this.state ={
+            selectedChat: null,
+            newChatFormVisible: false,
+            email: null,
+            chats: []
+        };
+    }
     render(){
-        return (<div>Hello World from Dashboard</div>);
+        return (
+            <div>
+                <ChatListComponent 
+                history={this.props.history}
+                newChatBtnFn={this.newChatBtnClicked}
+                selectChatFn={this.selectChat}
+                chats={this.state.chats}
+                userEmail={this.state.email}
+                selectedChatIndex={this.state.selectedChat}
+                ></ChatListComponent>   
+            </div>
+        );
+    }
+
+    selectChat = (chatIndex) => {
+        console.log('selected a chat', chatIndex);
+    }
+
+    newChatBtnClicked = () => this.setState({newChatFormVisible: true, selectedChat: null})
+    
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(async _usr => {
+            if (!_usr)
+                this.props.history.push('/login');
+            else{
+                await firebase
+                    .firestore()
+                    .collection('chats')
+                    .where('users', 'array-contains', _usr.email)
+                    .onSnapshot(async result => {
+                        const chats = result.docs.map(_doc => _doc.data());
+                        await this.setState({
+                            email: _usr.email,
+                            chats: chats
+                        });
+                        console.log(this.state);
+                    })
+            }
+        })
     }
 }
 
--- src/login/login.js
+++ src/login/login.js
@@ -1,9 +1,90 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
+import styles from './styles';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+const firebase = require("firebase");
 
 class LoginComponent extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            email: null,
+            password: null,
+            loginError: ''
+        };
+    }
+
     render(){
-        return (<div>Hello World from Login</div>);
+        const { classes } = this.props;
+
+        return (
+        <main className = { classes.main }>
+            <CssBaseline></CssBaseline>
+            <Paper className={classes.paper}>
+                <Typography component='h1' variant='h5'>
+                    Log in
+                </Typography>
+                <form className = {classes.form} onSubmit={(e) => this.submitLogin(e)}>
+                    <FormControl required fullWidth margin='normal'>
+                        <InputLabel htmlFor='login-email-input'>Enter Your Email</InputLabel>
+                        <Input autoComplete='email' autoFocus id='login-email-input' onChange={(e) => this.userTping('email', e)}></Input>
+                    </FormControl>
+                    <FormControl required fullWidth margin='normal'>
+                        <InputLabel htmlFor='login-password-input'>Enter Your Password</InputLabel>
+                        <Input type='password' id='login-password-input' onChange={(e) => this.userTping('password', e)}></Input>
+                    </FormControl>
+                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>Log In</Button>
+                </form>
+                {
+                    this.state.loginError ?
+                    <Typography component='h5' variant='h6' className={classes.errorText} >
+                        Incorrect Login Information
+                    </Typography> :
+                    null
+                }
+                <Typography component='h5' variant='h6' className={classes.noAccountHeader}>Don't have an account?</Typography>
+                <Link className={classes.signUpLink} to='/signup'> Sign Up!</Link>
+            </Paper>
+        </main>
+        );
+    }
+
+    userTping = (type, e) => {
+        switch (type){
+            case 'email':
+                this.setState({ email: e.target.value });
+                break;
+
+            case 'password':
+                this.setState({password: e.target.value});
+                break;
+
+            default:
+                break;
+        }
+        console.log(type, e);
+    }
+
+    submitLogin = (e) => {
+        e.preventDefault();
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => {
+                this.props.history.push('/dashboard');
+            }, err => {
+                this.setState({loginError: 'Server Error'});
+                console.log(err);
+            });
     }
 }
 
-export default LoginComponent;
+export default withStyles(styles)(LoginComponent);
